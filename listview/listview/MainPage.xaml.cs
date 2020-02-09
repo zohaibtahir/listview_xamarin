4ddb20b2489f3278435d2a3275538af6c4715b57
Zohaib Tahir <38902366+zohaibtahir@users.noreply.github.com>
2020-02-09 13:26:39 +0500

    Add project files.

---
+++ listview/listview/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using listview.Models;
+
+namespace listview
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            list.ItemsSource = new List<Contact>
+            {
+                new Contact{name="Saim",status="Hello I am their",imageUrl="http://lorempixel.com/100/100/people/1"},
+                new Contact{name="Naveed",status="Bussy",imageUrl="http://lorempixel.com/100/100/people/2"},
+                new Contact{name="Furqan",status="Active",imageUrl="http://lorempixel.com/100/100/people/3"},
+                new Contact{name="Muneeb",status="School",imageUrl="http://lorempixel.com/100/100/people/4"}  
+            };
+        }
+    }
+}
---
+++ listview/listview/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace listview.Models
+{
+    class Contact
+    {
+        public string name { get; set; }
+        public string status { get; set; }
+        public string imageUrl { get; set; }
+    }
+}
